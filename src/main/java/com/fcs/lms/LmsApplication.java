6cbb17161c04ba55dd134146aae0c4836746524f
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-12-03 15:48:49 +0700

    init project with appengine

--- src/main/java/com/fcs/lms/LmsApplication.java
+++ src/main/java/com/fcs/lms/LmsApplication.java
@@ -2,11 +2,17 @@ package com.fcs.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class LmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
 	}
+	@GetMapping("/")
+    public String hello() {
+            return "Hello Spring Boot! ";
+    }
 }
