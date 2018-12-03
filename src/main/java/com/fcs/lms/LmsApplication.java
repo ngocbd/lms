f47ddbff5cbb74176e1fbb48ec27491cce781eb3
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-12-03 17:33:27 +0700

    Working with firestore

--- src/main/java/com/fcs/lms/LmsApplication.java
+++ src/main/java/com/fcs/lms/LmsApplication.java
@@ -1,4 +1,15 @@
 package com.fcs.lms;
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +23,24 @@ public class LmsApplication {
 		SpringApplication.run(LmsApplication.class, args);
 	}
 	@GetMapping("/")
-    public String hello() {
-            return "Hello Spring Boot! ";
+    public String hello() throws IOException, InterruptedException, ExecutionException {
+		InputStream serviceAccount = LmsApplication.class.getResourceAsStream("freecodeschoollms-a70c7176ca7d.json");
+		GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+		FirebaseOptions options = new FirebaseOptions.Builder()
+		    .setCredentials(credentials)
+		    .build();
+		try {
+			FirebaseApp.initializeApp(options);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+
+		Firestore db = FirestoreClient.getFirestore();
+		ApiFuture<QuerySnapshot> query = db.collection("courses").get();
+		
+		return "Hello"+query.get().getDocuments().get(0).get("name");
     }
 }
+
+
