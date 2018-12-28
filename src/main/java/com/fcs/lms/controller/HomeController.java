6300e37e058e954b9e7c6ab160ca995ed1fe9456
lexuanduy <lexuanduy07@gmail.com>
2018-12-28 12:24:36 +0700

    get list course

--- src/main/java/com/fcs/lms/controller/HomeController.java
+++ src/main/java/com/fcs/lms/controller/HomeController.java
@@ -3,12 +3,17 @@ package com.fcs.lms.controller;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.fcs.lms.entity.Course;
+import com.fcs.lms.entity.Lecturer;
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.QuerySnapshot;
@@ -16,14 +21,28 @@ import com.google.cloud.firestore.QuerySnapshot;
 @Controller
 public class HomeController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
 	@GetMapping(value = "/")
 	public String index(Model model) throws InterruptedException, ExecutionException {
-
 		Firestore db = FirestoreOptions.getDefaultInstance().getService();
 		ApiFuture<QuerySnapshot> query = db.collection("courses").get();
+		ApiFuture<QuerySnapshot> queryLec = null;
+//		DocumentReference docRef;
+//		ApiFuture<DocumentSnapshot> lecturer = null;
 
 		List<Course> courses = query.get().toObjects(Course.class);
+		List<Lecturer> lecturers = null;
+		for (Course course : courses) {
+//			docRef = db.collection("lecturers").document(course.getLecturerId());
+//			lecturer = docRef.get();
+			queryLec = db.collection("lecturers").whereEqualTo("name", course.getAuthor()).get();
+			lecturers = queryLec.get().toObjects(Lecturer.class);
+		}
+
 		model.addAttribute("courses", courses);
+		LOGGER.info(lecturers.toString());
+		model.addAttribute("lecturers", lecturers);
 		return "views/home/index";
 	}
 
--- src/main/java/com/fcs/lms/entity/Course.java
+++ src/main/java/com/fcs/lms/entity/Course.java
@@ -4,61 +4,87 @@ public class Course {
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getIntro() {
 		return intro;
 	}
+
 	public void setIntro(String intro) {
 		this.intro = intro;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 	public String getObjective() {
 		return objective;
 	}
+
 	public void setObjective(String objective) {
 		this.objective = objective;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getDuration() {
 		return duration;
 	}
+
 	public void setDuration(int duration) {
 		this.duration = duration;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public String getPriceUnit() {
 		return priceUnit;
 	}
+
 	public void setPriceUnit(String priceUnit) {
 		this.priceUnit = priceUnit;
 	}
+
 	public int getHours() {
 		return hours;
 	}
+
 	public void setHours(int hours) {
 		this.hours = hours;
 	}
-	public String toString()
-	{
+
+	public String getLecturerId() {
+		return lecturerId;
+	}
+
+	public void setLecturerId(String lecturerId) {
+		this.lecturerId = lecturerId;
+	}
+
+	public String toString() {
 		return this.name;
 	}
+
 	private String name;
 	private String intro;
 	private String author;
@@ -68,5 +94,6 @@ public class Course {
 	private double price;
 	private String priceUnit;
 	private int hours;
-	
+	private String lecturerId;
+
 }
